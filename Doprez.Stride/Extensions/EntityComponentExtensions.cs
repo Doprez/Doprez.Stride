071e864bf92bd0faa8e11cfc4f7481081c2904fb
Doprez <73259914+Doprez@users.noreply.github.com>
2022-06-26 09:57:23 -0600

    added GetComponent extension to Entity to immitate funcionality from Unity

---
+++ Doprez.Stride/Extensions/EntityComponentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Stride.Engine;
+using System;
+using System.Linq;
+
+namespace Doprez.Stride.Extensions
+{
+	public static class EntityComponentExtensions
+	{
+		public static T GetComponent<T>(this Entity entity)
+		{
+			var type = typeof(T);
+
+			var result = entity.GetAll<EntityComponent>().OfType<T>().FirstOrDefault();
+
+			return result;
+		}
+	}
+}
