6ac89087efe40046dac73205a4e8924f3ea2cddc
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-07 20:31:26 -0700

    added a way to quickly calculate height based on an attached modelComponent model

---
+++ Doprez.Stride/Extensions/ModelComponentExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Stride.Engine;
+
+namespace Stride.Engine;
+
+public static class ModelComponentExtensions
+{
+	public static float GetMeshHeight(this ModelComponent modelComponent)
+	{
+		var boundingBox = modelComponent.Model.BoundingBox;
+		var height = boundingBox.Maximum.Y - boundingBox.Minimum.Y;
+		return height;
+	}
+}
