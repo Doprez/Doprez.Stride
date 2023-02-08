2b3a3e7f722903b5adefc201bd3b57c02be61077
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-07 20:30:22 -0700

    rewrote the AsyncFiniteStateMachine

--- Doprez.Stride/StateMachines/AsyncFiniteStateMachine.cs
+++ Doprez.Stride/StateMachines/AsyncFiniteStateMachine.cs
@@ -10,36 +10,40 @@ namespace Doprez.Stride.StateMachines
 {
 
     [ComponentCategory("FSM")]
-    public abstract class AsyncFiniteStateMachine : AsyncScript
+    public class AsyncFiniteStateMachine : AsyncScript
 	{
 		/// <summary>
 		/// All states that can be accessed by the FSM
 		/// </summary>
 		[DataMemberIgnore]
-        public Dictionary<int, IState> States = new Dictionary<int, IState>();
+        public Dictionary<int, IAsyncState> States = new Dictionary<int, IAsyncState>();
 
-        protected IState _currentState;
+        protected IAsyncState _currentState;
 
-        public AsyncFiniteStateMachine()
-        {
-		}
-
-        public void Add(int id, IState state)
+        public void Add(int id, IAsyncState state)
         {
             States.Add(id, state);
         }
 
-        public IState GetActiveState()
+		public override async Task Execute()
+		{
+            while (Game.IsRunning)
+            {
+                await _currentState.UpdateState();
+            }
+		}
+
+		public IAsyncState GetActiveState()
         {
             return _currentState;
         }
 
-        public IState GetState(int id)
+        public IAsyncState GetState(int id)
 		{
             return States[id];
 		}
 
-        public void SetCurrentState(IState state)
+        public void SetCurrentState(IAsyncState state)
         {
             if (_currentState != null)
             {
