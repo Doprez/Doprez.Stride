e9275372f014bf8880b93537d8d5faddcc5656df
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-07 20:30:56 -0700

    Added IAsyncState for the async fsm to use

---
+++ Doprez.Stride/StateMachines/IAsyncState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doprez.Stride.StateMachines;
+public interface IAsyncState
+{
+
+	public string Name { get; set; }
+	public AsyncFiniteStateMachine FiniteStateMachine { get; set; }
+	public bool IsDefaultState { get; set; }
+
+	public Task EnterState();
+	public Task ExitState();
+	public Task UpdateState();
+
+}
