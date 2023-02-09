3a93987913e7a4063bff5859f4b98bc054f8ee2a
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-08 21:08:03 -0700

    added description for GetMeshHeight

--- Doprez.Stride/Extensions/ModelComponentExtensions.cs
+++ Doprez.Stride/Extensions/ModelComponentExtensions.cs
@@ -4,6 +4,11 @@ namespace Stride.Engine;
 
 public static class ModelComponentExtensions
 {
+	/// <summary>
+	/// Gets the ModelComponents BoundingBox and calculates the Y height
+	/// </summary>
+	/// <param name="modelComponent"></param>
+	/// <returns></returns>
 	public static float GetMeshHeight(this ModelComponent modelComponent)
 	{
 		var boundingBox = modelComponent.Model.BoundingBox;
