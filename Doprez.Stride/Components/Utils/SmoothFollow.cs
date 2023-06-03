002bcae65c8b48f13966a79ed35b32366fdf4078
Doprez <73259914+Doprez@users.noreply.github.com>
2023-06-03 12:03:17 -0600

    added follow and rotate scripts for new utilities

---
+++ Doprez.Stride/Components/Utils/SmoothFollow.cs
@@ -0,0 +1,24 @@
+﻿using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+
+namespace Doprez.Stride.Utils;
+
+[DataContract("DoprezSmoothFollow")]
+[ComponentCategory("DoprezUtils")]
+public class SmoothFollow : SyncScript
+{
+
+	public Entity EntityToFollow { get; set; }
+	public Vector3 Speed { get; set; } = new Vector3(1, 1, 1);
+
+	public override void Update()
+	{
+		var currentPosition = Entity.Transform.Position;
+		var otherPosition = EntityToFollow.WorldPosition();
+		var x = MathUtil.Lerp(currentPosition.X, otherPosition.X, Speed.X * this.DeltaTime());
+		var y = MathUtil.Lerp(currentPosition.Y, otherPosition.Y, Speed.Y * this.DeltaTime());
+		var z = MathUtil.Lerp(currentPosition.Z, otherPosition.Z, Speed.Z * this.DeltaTime());
+		Entity.Transform.Position = new Vector3(x, y, z);
+	}
+}
---
+++ Doprez.Stride/Components/Utils/SmoothRotate.cs
@@ -0,0 +1,24 @@
+﻿using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+
+namespace Doprez.Stride.Utils;
+
+[DataContract("DoprezSmoothRotate")]
+[ComponentCategory("DoprezUtils")]
+public class SmoothRotate : SyncScript
+{
+
+	public Entity EntityToFollow { get; set; }
+	public Vector3 Speed { get; set; } = new Vector3(1, 1, 1);
+
+	public override void Update()
+	{
+		var currentRotation = Entity.Transform.Rotation;
+		EntityToFollow.Transform.GetWorldTransformation(out var _, out var otherRotation, out var _);
+
+		Quaternion.Slerp(ref currentRotation, ref otherRotation, Speed.X * this.DeltaTime(), out var newRotation);
+
+		Entity.Transform.Rotation = newRotation;
+	}
+}
