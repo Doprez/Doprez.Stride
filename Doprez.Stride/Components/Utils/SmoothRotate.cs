c591ff72475991cd3a5f23eee64e5adfc7731900
Doprez <73259914+Doprez@users.noreply.github.com>
2023-06-08 11:25:27 -0600

    clean up

--- Doprez.Stride/Components/Utils/SmoothRotate.cs
+++ Doprez.Stride/Components/Utils/SmoothRotate.cs
@@ -5,18 +5,18 @@ using Stride.Engine;
 namespace Doprez.Stride.Utils;
 
 [DataContract("DoprezSmoothRotate")]
-[ComponentCategory("DoprezUtils")]
+[ComponentCategory("Utils")]
 public class SmoothRotate : SyncScript
 {
 	public Entity EntityToFollow { get; set; }
-	public Vector3 Speed { get; set; } = new Vector3(1, 1, 1);
+	public float Speed { get; set; } = 1;
 
 	public override void Update()
 	{
 		var currentRotation = Entity.Transform.Rotation;
 		EntityToFollow.Transform.GetWorldTransformation(out var _, out var otherRotation, out var _);
 
-		Quaternion.Slerp(ref currentRotation, ref otherRotation, Speed.X * this.DeltaTime(), out var newRotation);
+		Quaternion.Slerp(ref currentRotation, ref otherRotation, Speed * this.DeltaTime(), out var newRotation);
 
 		Entity.Transform.Rotation = newRotation;
 	}
