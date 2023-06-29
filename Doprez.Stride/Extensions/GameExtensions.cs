5d2c657d5f0babd3f67874de26681428d0df8dcf
Doprez <73259914+Doprez@users.noreply.github.com>
2023-06-28 23:32:02 -0600

    added fps extension

---
+++ Doprez.Stride/Extensions/GameExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Stride.Engine;
+
+namespace Stride.Engine;
+
+public static class GameExtensions
+{
+	public static float FPS(this Game game)
+	{
+		return game.UpdateTime.FramePerSecond;
+	}
+}
